65a5fff53069910d0f2609d74149986b3af5b427
Rcaceres2 <reynaldocaceres0@gmail.com>
2025-05-29 20:28:51 -0400

    colorverde

--- practica1.cpp
+++ practica1.cpp
@@ -2,8 +2,18 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <windows.h> 
 
 using namespace std;
+
+void textoVerde() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+}
+void resetColor() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+}
 
 int main() {
     string nombreArchivo = "texto.txt";
@@ -20,36 +30,55 @@ int main() {
     transform(palabra.begin(), palabra.end(), palabra.begin(), ::tolower);
     
     string linea;
+    int nLinea = 0;
     int total = 0;
-    bool repetidos = false;
+    bool repetidos = false;  
 
     cout << "\nResultados de la busqueda:\n" << endl;
     
     while (getline(archivo, linea)) {
+        nLinea++;
+        string lineaOriginal = linea; 
         string lineaMinusculas = linea;
         transform(lineaMinusculas.begin(), lineaMinusculas.end(), lineaMinusculas.begin(), ::tolower);
         
         size_t pos = 0;
+        size_t lastPos = 0;
         bool lRepetidas = false;
         
         while ((pos = lineaMinusculas.find(palabra, pos)) != string::npos) {
             if (!lRepetidas) {
+                cout << "Linea " << nLinea << ": ";
                 lRepetidas = true;
-                repetidos = true;
+                repetidos = true;  // Actualizado para usar 'repetidos'
             }
+            
+            cout << lineaOriginal.substr(lastPos, pos - lastPos);
+            
+            textoVerde();
+            cout << lineaOriginal.substr(pos, palabra.length());
+            resetColor();
+            
             total++;
             pos += palabra.length();
+            lastPos = pos;
         }
 
         if (lRepetidas) {
-            cout << linea << endl;
+            if (lastPos < lineaOriginal.length()) {
+                cout << lineaOriginal.substr(lastPos);
+            }
+            cout << endl;
         }
     }
     
-    if (!repetidos) {
+    if (!repetidos) {  
         cout << "No se encontraron repeticiones." << endl;
     } else {
-        cout << "\nTotal de repeticiones encontradas: " << total << endl;
+        cout << "\nTotal de repeticiones encontradas: ";
+        textoVerde();
+        cout << total << endl;
+        resetColor();
     }
     
     archivo.close();
